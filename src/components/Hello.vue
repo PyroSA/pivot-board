f06495bc3a60b1b7f353de6cd6c8926c2a7d7151
Werner Ackermann <pyros911@gmail.com>
2017-06-14 22:18:30 +0200

    add all the webkit project files

---
+++ src/components/Hello.vue
@@ -0,0 +1,358 @@
+<template>
+  <div class="hello">
+    <div v-show="false">Loading...</div>
+    <section class="serverapp" v-cloak>
+      <header class="header">
+        <nav class="navbar navbar-toggleable-sm navbar-light bg-faded">
+          <h1 class="navbar-brand">Pivotal Board</h1>
+
+          <ul class="nav nav-pills" v-show="projects">
+            <li class="nav-item" v-for="(project, index) in projects" :key="project.id">
+              <a class="nav-link" :class="{ active: project == selectedProject }" href="#" @click="selectProject(project)">{{project.name}}</a>
+            </li>
+          </ul>
+
+          <input class="form-control" v-show="!connected"
+            autofocus autocomplete="off"
+            placeholder="Pivotal API Token"
+            v-model="config.pivotToken"
+            @keyup.enter="setToken()"
+          >
+
+          <div class="ml-auto justify-content-end">
+            <button class="btn btn-primary" v-show="!connected"
+              type="button"
+              @click="setToken()">
+                Connect
+            </button>
+
+            <button class="btn btn-secondary" v-show="connected"
+              type="button"
+              @click="disconnect()">
+                Disconnect
+            </button>
+          </div>
+        </nav>
+      </header>
+
+      <main>
+        <div class='row' v-show="!connected">
+          <div class='col-sm-12'>
+            <a href="https://www.pivotaltracker.com/profile">Get your API Token from your PivotalTracker Profile page</a>
+          </div>
+        </div>
+        <div class="alert alert-danger" role="alert" v-show="error.possible_fix">
+          <strong>{{ error.error}}</strong> {{ error.possible_fix}}
+        </div>
+        <div class="alert alert-danger" role="alert" v-show="error.message">
+          <strong>{{ error.name }}</strong> {{ error.message }}
+        </div>
+
+        <div v-show="stories">
+          <h1>Stories</h1>
+          <div class="row">
+            <div class="col-sm-3">
+              <h2>Todo</h2>
+              <div class="card" v-for="(story, index) in todoStories" :key="story.id" :class="cardStyle(story)">
+                <div class="card-block">
+                  <i class="fa fa-envelope fa-5x fa-border icon-grey badge">5</i>
+                  <h4 class="card-title">{{story.story_type}}
+                    <span class="badge badge-primary" v-show="story.estimate">{{story.estimate}}</span>
+                    <a :href="story.url">{{story.id}}</a>
+                  </h4>
+                  <p class="card-text">{{story.name}}</p>
+                  <div></div>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-3">
+              <h2>Dev</h2>
+              <div class="card" v-for="(story, index) in devStories" :key="story.id" :class="cardStyle(story)">
+                <div class="card-block">
+                  <h4 class="card-title">{{story.story_type}}
+                    <span class="badge badge-primary" v-show="story.estimate">{{story.estimate}}</span>
+                    <a :href="story.url">{{story.id}}</a>
+                  </h4>
+                  <p class="card-text">{{story.name}}</p>
+                  <div></div>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-3">
+              <h2>QA</h2>
+              <div class="card" v-for="(story, index) in qaStories" :key="story.id" :class="cardStyle(story)">
+                <div class="card-block">
+                  <h4 class="card-title">{{story.story_type}}
+                    <span class="badge badge-info" v-show="story.estimate">{{story.estimate}}</span>
+                    <a :href="story.url">{{story.id}}</a>
+                  </h4>
+                  <p class="card-text">{{story.name}}</p>
+                  <div></div>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-3">
+              <h2>Done</h2>
+              <div class="card" v-for="(story, index) in doneStories" :key="story.id" :class="cardStyle(story)">
+                <div class="card-block">
+                  <h4 class="card-title">{{story.story_type}}
+                    <span class="badge badge-primary" v-show="story.estimate">{{story.estimate}}</span>
+                    <a :href="story.url">{{story.id}}</a>
+                  </h4>
+                  <p class="card-text">{{story.name}}</p>
+                  <div></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <!--<footer class="footer">
+        <span>Footer</span>
+      </footer>-->
+    </section>
+  </div>
+</template>
+
+<script>
+import Pivotal from '../lib/pivotalJs';
+import ConfigStorage from '../lib/configStorage';
+import moment from 'moment';
+import _ from 'lodash';
+
+const CONFIG_STORAGE_KEY = 'pivot-board';
+const configStorage = new ConfigStorage(CONFIG_STORAGE_KEY);
+
+const mainChart = new window.Chartist.Line('.ct-chart', { labels: ['new'], series: [[0, 30, 60, 90, 0, 100]] });
+
+const buildIterationGraph = (iteration) => {
+  const dailyPoints = {
+    labels: [],
+    created: [],
+    updated: [],
+    accepted: [],
+    todo: [],
+    burn: []
+  };
+
+  let day;
+
+  for (day = iteration.start; day <= iteration.finish; day += 86400000) {
+    dailyPoints.labels.push(moment(day).format('D MMM'));
+    dailyPoints.created.push(iteration.stories.reduce((acc, story) => {
+      return story.created_at <= day ? acc + (story.estimate || 0) : acc;
+    }, 0));
+    dailyPoints.updated.push(iteration.stories.reduce((acc, story) => {
+      return story.updated_at <= day ? acc + (story.estimate || 0) : acc;
+    }, 0));
+    dailyPoints.accepted.push(iteration.stories.reduce((acc, story) => {
+      return story.accepted_at <= day ? acc + (story.estimate || 0) : acc;
+    }, 0));
+    dailyPoints.todo.push(
+      dailyPoints.created[dailyPoints.created.length - 1] -
+      dailyPoints.accepted[dailyPoints.accepted.length - 1]
+    );
+  }
+
+  let days = -1;
+  for (day = iteration.finish; day >= iteration.start; day -= 86400000) {
+    const weekday = moment(day).weekday();
+    if (weekday > 0 && weekday < 6) {
+      days++;
+    }
+    dailyPoints.burn.unshift(days < 0 ? 0 : days);
+  }
+
+  const totalDaysDiv = 1 / dailyPoints.burn[0];
+  for (day = 0; day < dailyPoints.burn.length; day++) {
+    console.log(day, dailyPoints.burn[day]);
+    dailyPoints.burn[day] = dailyPoints.created[day] * dailyPoints.burn[day] * totalDaysDiv;
+  }
+
+  const data = {
+    labels: dailyPoints.labels,
+    series: [
+      dailyPoints.created,
+      // dailyPoints.updated,
+      // dailyPoints.accepted,
+      dailyPoints.burn,
+      dailyPoints.todo
+    ]
+  };
+  mainChart.update(data);
+};
+
+export default {
+  name: 'hello',
+  data () {
+    return {
+      config: configStorage.load(),
+      connected: false,
+      pivotal: undefined,
+      error: '',
+      projects: undefined,
+      stories: undefined,
+      selectedProject: undefined
+    };
+  },
+  computed: {
+    monitoring: function () {
+      return this.config.connect;
+    },
+    todoStories: function () {
+      return (this.stories || []).filter(function (story) {
+        return ['planned', 'rejected'].includes(story.current_state);
+      });
+    },
+    devStories: function () {
+      return (this.stories || []).filter(function (story) {
+        return ['started', 'finished'].includes(story.current_state);
+      });
+    },
+    qaStories: function () {
+      return (this.stories || []).filter(function (story) {
+        return ['delivered'].includes(story.current_state);
+      });
+    },
+    doneStories: function () {
+      return (this.stories || []).filter(function (story) {
+        return ['accepted'].includes(story.current_state);
+      });
+    }
+  },
+  methods: {
+    setToken: function () {
+      configStorage.save(this.config);
+      this.connect();
+    },
+    cardStyle: function (story) {
+      switch (story.current_state) {
+        case 'planned': return 'card-outline-primary';
+        case 'rejected': return 'card-outline-danger';
+        case 'started': return 'card-outline-primary';
+        case 'finished': return 'card-outline-warning';
+        case 'delivered': return 'card-outline-primary';
+        case 'accepted': return 'card-outline-success';
+        default: return 'card-outline-secondary';
+      }
+    },
+    disconnect: function () {
+      this.connected = false;
+      this.projects = undefined;
+      this.stories = undefined;
+      this.selectProject(undefined);
+    },
+    selectIteration: function (iteration) {
+      console.log('Iteration', iteration);
+      if (iteration) {
+        this.stories = iteration.stories;
+
+        buildIterationGraph(iteration);
+
+        const storyCount = {};
+        this.stories.forEach(function (story) {
+          storyCount[story.current_state] = (storyCount[story.current_state] || 0) + 1;
+        });
+        console.log(storyCount);
+      } else {
+        this.stories = undefined;
+      }
+    },
+    selectProject: function (project) {
+      console.log('Project', project);
+      this.selectedProject = project;
+      this.selectIteration(undefined);
+      if (project) {
+        // this.pivotal.getCurrentIterations(project.id, (err, iterations) => {
+        //   console.log('cb');
+        //   if (err) {
+        //     console.error('selectedProject', err);
+        //     return;
+        //   }
+        //   const all = {
+        //     start: iterations[0].start,
+        //     finish: iterations[0].finish,
+        //     stories: []
+        //   };
+        //   iterations.forEach(function (iteration) {
+        //     console.log(all);
+        //     console.log(iteration);
+        //     all.start = Math.min(all.start, iteration.start);
+        //     all.finish = Math.max(all.finish, iteration.finish);
+        //     all.stories = all.stories.concat(iteration.stories);
+        //   });
+        //   this.selectIteration(all);
+        // });
+
+        this.pivotal.getIterations(project.id, { scope: 'done_current', date_format: 'millis', offset: -3 }, (iterations) => {
+          console.log('comp', iterations);
+          if (_.isError(iterations)) {
+            console.error('selectedProject', iterations);
+            return;
+          }
+          const all = {
+            start: iterations[0].start,
+            finish: iterations[0].finish,
+            stories: []
+          };
+          iterations.forEach(function (iteration) {
+            console.log(all);
+            console.log(iteration);
+            all.start = Math.min(all.start, iteration.start);
+            all.finish = Math.max(all.finish, iteration.finish);
+            all.stories = all.stories.concat(iteration.stories);
+          });
+          this.selectIteration(all);
+        });
+      }
+    },
+    connect: function () {
+      if (!this.config.pivotToken) {
+        this.disconnect();
+        return;
+      }
+      this.error = {};
+      this.connected = true;
+      this.pivotal = new Pivotal(this.config.pivotToken);
+      this.pivotal.getProjects((err, projects) => {
+        if (err || projects.error) {
+          this.error = err || projects;
+          console.error(this.error);
+          this.disconnect();
+          return;
+        }
+        if (projects && projects.length > 0) {
+          console.log(projects);
+          this.projects = projects;
+          this.selectProject(projects[0]);
+        }
+      });
+    }
+  },
+  mounted () {
+    this.disconnect();
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/lib/pivotalJs.js
@@ -0,0 +1,345 @@
+import querystring from 'querystring';
+import _ from 'lodash';
+
+let Pivotal;
+
+/**
+ * Pivotal API Interface
+ * @constructor
+ * @param {string} apiToken Pivotal API Token
+ */
+Pivotal = function Pivotal (apiToken) {
+  this.apiToken = apiToken;
+  this.baseUrl = 'https://www.pivotaltracker.com/services/v5/';
+};
+
+/**
+ * Update a story
+ * @param  {String}   projectId Pivotal project id
+ * @param  {String}   storyId   Pivotal story id
+ * @param  {Object}   [params]  Extra parameters
+ * @param  {Function} [callback]  function(error, response)
+ */
+Pivotal.prototype.updateStory = function updateStory (projectId, storyId, params, callback) {
+  this.api('put', 'projects/' + projectId + '/stories/' + storyId, {
+    json: params
+  }, callback);
+};
+
+/**
+ * Obtains a story
+ * @param  {String}   storyId   Pivotal story id
+ * @param  {Function} [callback]  function(error, response)
+ */
+Pivotal.prototype.getStory = function getStory (storyId, callback) {
+  this.api('get', '/stories/' + storyId, {}, callback);
+};
+
+/**
+ * Obtains list of projects
+ * @param  {Function} [callback]  function(error, projects)
+ */
+Pivotal.prototype.getProjects = function getProjects (callback) {
+  this.api('get', '/projects', {}, callback);
+};
+
+/**
+ * Get paginated stories from project
+ * @param  {String}   projectId   Pivotal project id
+ * @param  {Object}   [options]   Extra options
+ * @param  {Function} [callback]  function(stories, pagination, callback(error or true to stop pagination))
+ * @param  {Function} [completed] function(error)
+ * @param  {Integer}  [offset]    Initial pagination offset
+ * @param  {Integer}  [limit]     Pagination limit for each response
+ */
+Pivotal.prototype.getStories = function getStories (projectId, options, callback, completed, offset, limit) {
+  this.paginated('projects/' + projectId + '/stories', offset || 0, limit || 128, options, callback, completed);
+};
+
+/**
+ * Get paginated activity from project
+ * @param  {String}   projectId   Pivotal project id
+ * @param  {Object}   [options]   Extra options
+ * @param  {Function} [callback]  function(events, pagination, callback(error or true to stop pagination))
+ * @param  {Function} [completed] function(error)
+ * @param  {Integer}  [offset]    Initial pagination offset
+ * @param  {Integer}  [limit]     Pagination limit for each response
+ */
+Pivotal.prototype.getActivity = function getActivity (projectId, options, callback, completed, offset, limit) {
+  this.paginated('projects/' + projectId + '/activity', offset || 0, limit || 128, options, callback, completed);
+};
+
+/**
+ * Get activity for story
+ * @param  {String}   projectId Pivotal project id
+ * @param  {String}   storyId   Pivotal story id
+ * @param  {Function} [callback]  function(error, events)
+ * @param  {Object}   [options]   Extra options
+ */
+Pivotal.prototype.getStoryActivity = function getStoryActivity (projectId, storyId, callback, options) {
+  this.api('get', 'projects/' + projectId + '/stories/' + storyId + '/activity', {
+    qs: options || {}
+  }, callback);
+};
+
+/**
+ * Get API token owner activity
+ * @param  {Function} [callback]  function(error, events)
+ * @param  {Object}   [options]   Extra options
+ */
+Pivotal.prototype.getMyActivity = function getMyActivity (callback, options) {
+  this.api('get', 'my/activity', {
+    qs: options || {}
+  }, callback);
+};
+
+/**
+ * Get tasks for story
+ * @param  {String}   projectId Pivotal project id
+ * @param  {String}   storyId   Pivotal story id
+ * @param  {Function} [callback]  function(error, tasks)
+ */
+Pivotal.prototype.getTasks = function getTasks (projectId, storyId, callback) {
+  this.api('get', 'projects/' + projectId + '/stories/' + storyId + '/tasks', {}, callback);
+};
+
+/**
+ * Add story task
+ * @param  {String}   projectId   Pivotal project id
+ * @param  {String}   storyId     Pivotal story id
+ * @param  {String}   description Description
+ * @param  {int}      position    Position
+ * @param  {Function} [callback]  function(error, label)
+ */
+Pivotal.prototype.addTask = function addTask (projectId, storyId, description, position, callback) {
+  this.api('post', 'projects/' + projectId + '/stories/' + storyId + '/tasks', {
+    body: {
+      description: description,
+      position: position
+    }
+  }, callback);
+};
+
+/**
+ * Get paginated iterations for project
+ * @param  {String}   projectId   Pivotal project id
+ * @param  {Object}   [options]   Extra options
+ * @param  {Function} [callback]  function(iterations, pagination, callback(error or true to stop pagination))
+ * @param  {Function} [completed] function(error)
+ * @param  {Integer}  [offset]    Initial pagination offset
+ * @param  {Integer}  [limit]     Pagination limit for each response
+ */
+Pivotal.prototype.getIterations = function getIterations (projectId, options, callback, completed, offset, limit) {
+  this.paginated('projects/' + projectId + '/iterations', offset || 0, limit || 128, options, callback, completed);
+};
+
+/**
+ * Get current iteration stories from project
+ * @param  {String} projectId Pivotal project id
+ * @param  {Function} [callback]  function(error, iterations)
+ */
+Pivotal.prototype.getCurrentIterations = function (projectId, callback) {
+  this.api('get', 'projects/' + projectId + '/iterations', {
+    qs: {
+      scope: 'current',
+      date_format: 'millis'
+    }
+  }, function (err, iterations) {
+    if (_.isFunction(callback)) {
+      if (err || !iterations) {
+        callback(err);
+      } else {
+        callback(false, iterations);
+      }
+    }
+  });
+};
+
+/**
+ * Get memberships for project
+ * @param  {String}   projectId Pivotal project id
+ * @param  {Function} [callback]  function(error, memberships)
+ */
+Pivotal.prototype.getMemberships = function updateStory (projectId, callback) {
+  this.api('get', 'projects/' + projectId + '/memberships', {}, callback);
+};
+
+/**
+ * Get comments from story
+ * @param  {String}   projectId Pivotal project id
+ * @param  {String}   storyId   Pivotal story id
+ * @param  {Function} [callback]  function(error, comments)
+ */
+Pivotal.prototype.getComments = function getStories (projectId, storyId, callback) {
+  this.api('get', 'projects/' + projectId + '/stories/' + storyId + '/comments', {}, callback);
+};
+
+/**
+ * Export stories from Pivotal
+ * @param  {String[]}  stories   List of story id's
+ * @param  {Function}  [callback]  function(error, response)
+ */
+Pivotal.prototype.exportStories = function exportStories (stories, callback) {
+  this.api('post', 'stories/export', {
+    body: querystring.stringify({
+      'ids[]': stories
+    })
+  }, callback);
+};
+
+// /**
+//  * Post attachment to story
+//  * @param  {String}   projectId Pivotal project id
+//  * @param  {String}   storyId   Pivotal story id
+//  * @param  {String}   content   Attachment content
+//  * @param  {String}   filename  Attachment filename
+//  * @param  {String}   type      Content type of attachment
+//  * @param  {String}   comment   Comment text
+//  * @param  {Function} [callback]  function(error, response)
+//  */
+// Pivotal.prototype.postAttachment = function postAttachment (projectId, storyId, content, filename, type, comment, callback) {
+//   var that = this;
+//   request.post({
+//     url: `https://www.pivotaltracker.com/services/v5/projects/${projectId}/uploads`,
+//     multipart: [{
+//       'Content-Disposition': `form-data; name="file"; filename="${filename}"`,
+//       'Content-Type': type,
+//       'body': content
+//     }],
+//     headers: {
+//       'X-TrackerToken': that.apiToken
+//     }
+//   }, function (err, res, upload) {
+//     if (err || upload.kind === 'error') {
+//       callback(err, {
+//         success: false,
+//         error: upload ? upload.error + ' (' + upload.general_problem + ')' : err
+//       });
+//     } else {
+//       that.api('post', 'projects/' + projectId + '/stories/' + storyId + '/comments', {
+//         json: {
+//           text: comment,
+//           file_attachments: [JSON.parse(upload)]
+//         }
+//       }, callback);
+//     }
+//   });
+// };
+
+/**
+ * Get all labels in project
+ * @param  {String}   projectId Pivotal project id
+ * @param  {Function} [callback]  function(error, labels)
+ */
+Pivotal.prototype.getLabels = function getLabels (projectId, callback) {
+  this.api('get', 'projects/' + projectId + '/labels', {}, callback);
+};
+
+/**
+ * Get labels on story
+ * @param  {String}   projectId Pivotal project id
+ * @param  {String}   storyId   Pivotal story id
+ * @param  {Function} [callback]  function(error, labels)
+ */
+Pivotal.prototype.getStoryLabels = function getStoryLabels (projectId, storyId, callback) {
+  this.api('get', 'projects/' + projectId + '/stories/' + storyId + '/labels', {}, callback);
+};
+
+/**
+ * Add story label
+ * @param  {String}   projectId Pivotal project id
+ * @param  {String}   storyId   Pivotal story id
+ * @param  {String}   name      Name of label
+ * @param  {Function} [callback]  function(error, label)
+ */
+Pivotal.prototype.addStoryLabel = function addStoryLabel (projectId, storyId, name, callback) {
+  this.api('post', 'projects/' + projectId + '/stories/' + storyId + '/labels', {
+    body: {
+      name: name
+    }
+  }, callback);
+};
+
+/**
+ * Create label
+ * @param  {String}   projectId Pivotal project id
+ * @param  {String}   name      Name of label
+ * @param  {Function} [callback]  function(error, label)
+ */
+Pivotal.prototype.createLabel = function createStory (projectId, name, callback) {
+  this.api('post', 'projects/' + projectId + '/labels', {
+    body: {
+      name: name
+    }
+  }, callback);
+};
+
+/**
+ * Create new Pivotal story
+ * @param  {String}   projectId Pivotal project id
+ * @param  {Object}   [params]  Story parameters
+ * @param  {Function} [callback]  function(error, story)
+ */
+Pivotal.prototype.createStory = function createStory (projectId, params, callback) {
+  this.api('post', 'projects/' + projectId + '/stories', {
+    body: params
+  }, callback);
+};
+
+Pivotal.prototype.paginated = function (path, offset, limit, options, callback, completed) {
+  var that = this;
+  var _options = {
+    qs: _.extend({
+      offset: offset,
+      limit: limit,
+      envelope: true
+    }, options)
+  };
+  this.api('get', path, _options, function (err, res) {
+    if (err || !res.pagination) {
+      completed && completed(err || res);
+    } else {
+      offset += res.pagination.returned;
+      callback && callback(res.data, res.pagination, function (err) {
+        if (!err) {
+          var left = res.pagination.total - offset;
+          if (left > 0) {
+            that.paginated(path, offset, Math.min(left, limit), options, callback, completed);
+          } else {
+            completed && completed();
+          }
+        } else {
+          completed && completed(err);
+        }
+      });
+    }
+  });
+};
+
+Pivotal.prototype.api = function api (method, path, options, callback) {
+  const xhr = new window.XMLHttpRequest();
+  console.log('API', method, path, options);
+  if (options.qs) {
+    xhr.open(method, `${this.baseUrl}${path}?${querystring.stringify(options.qs)}`);
+  } else {
+    xhr.open(method, `${this.baseUrl}${path}`);
+  }
+  xhr.responseType = 'json';
+  xhr.setRequestHeader('X-TrackerToken', this.apiToken);
+
+  xhr.onload = function () {
+    if (_.isFunction(callback)) {
+      callback(undefined, xhr.response);
+    }
+  };
+
+  xhr.onerror = function (err) {
+    console.error("Houston, we've got a problem.", err, xhr);
+    if (_.isFunction(callback)) {
+      callback(err);
+    }
+  };
+  xhr.send();
+};
+
+export default Pivotal;
